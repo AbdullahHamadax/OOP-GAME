25b9d475b4281ccd3116ccf480771011bba4648c
waryoyo <youssef_hus@hotmail.com>
2023-12-10 13:53:46 +0200

    no one can make utility

--- src/Classes/Utility.java
+++ src/Classes/Utility.java
@@ -28,6 +28,8 @@ public class Utility {
         }
     }
 
+    private Utility() {}
+
     public static void clearTerminal() {
         System.out.print("\033[H\033[2J");
 //        System.out.flush();

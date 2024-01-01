74c0ad12a0dfed6d86cfa9c80b7d116d50d44a0c
Xdev-ops32 <147554618+Xdev-ops32@users.noreply.github.com>
2024-01-01 15:42:09 +0200

    elemental moves done,ig

--- src/classes/core/Move.java
+++ src/classes/core/Move.java
@@ -6,6 +6,9 @@ import classes.entity.Character;
 public class Move {
     private String name;
     private int power, accuracy, type;
+    public enum ElementalMovements{
+        FIRE ,EARTH ,BLOOD ,AIR
+        ,THUNDER , WATER}
 
     public Move(String name, int power, int type, int accuracy) {
         this.name = name;
@@ -55,5 +58,44 @@ public class Move {
 
     public void setType(int type) {
         this.type = type;
+    }
+
+    public static int damageElementEvaluator(ElementalMovements player , ElementalMovements enemy){
+        int baseDamage = 5;
+        switch(player){
+            case AIR:
+            if(enemy == ElementalMovements.EARTH){
+                    return baseDamage * 10;
+                }
+                break;
+            case FIRE:
+                if(enemy == ElementalMovements.WATER){
+                    return baseDamage * 10;
+                }
+                break ;
+            case BLOOD:
+                if(enemy == ElementalMovements.THUNDER){
+                    return baseDamage * 10 ;
+                }
+                break;
+            case EARTH:
+                if(enemy == ElementalMovements.AIR){
+                    return baseDamage * 10 ;
+                }
+                break;
+            case WATER:
+                if(enemy == ElementalMovements.FIRE){
+                    return baseDamage * 10 ;
+                }
+                break;
+            case THUNDER:
+                if(enemy == ElementalMovements.BLOOD){
+                    return  baseDamage * 10 ;
+                }
+                break;
+            default:
+                System.out.print("Neutral damage");
+        }
+        return baseDamage;
     }
 }
